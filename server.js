bdaa2e820d3596d8407e7eee5388bdd2564bbd9d
chachora <chachora@gmail.com>
2018-02-04 13:19:43 -0800

    Add configuration

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const http = require('http');
 const httpProxy = require('http-proxy');
+const config = require('config');
+const PORT = config.port;
 
 //
 // Create your proxy server and set the target in the options.
 //
-httpProxy.createProxyServer({target: 'http://localhost:9000'}).listen(8000);
+const proxy = httpProxy.createProxyServer({});
 
 //
 // Create your target server
 //
 http.createServer(function (req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write('request successfully proxied!' + '\n' + JSON.stringify(req.headers, true, 2));
-  res.end();
-}).listen(9000);
+  proxy.web(req, res);
+}).listen(PORT);
+
+console.log(`Server started at ${PORT}`);
