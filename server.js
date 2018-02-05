13ddcb48976f806a934df2e54fde968114b1350b
chachora <chachora@gmail.com>
2018-02-05 11:06:08 -0800

    Code refactoring

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 const http = require('http');
 const httpProxy = require('http-proxy');
 const config = require('config');
-const PORT = config.port;
+const port = config.port;
+const proxyOptions = {...config.proxy};
 
 //
 // Create your proxy server and set the target in the options.
 //
-const proxy = httpProxy.createProxyServer({});
+const proxy = httpProxy.createProxyServer(proxyOptions);
 
 //
 // Create your target server
 //
 http.createServer(function (req, res) {
   proxy.web(req, res);
-}).listen(PORT);
+}).listen(port);
 
-console.log(`Server started at ${PORT}`);
+console.log(`Server started at ${port}`);
