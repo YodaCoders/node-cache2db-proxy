744e5ecb73742552b7f39487af385876f788e9b2
chachora <chachora@gmail.com>
2018-03-19 14:37:14 -0700

    Add checkTtl option, handle target server errors correctly

--- server.js
+++ server.js
@@ -1,40 +1,43 @@
-const http = require('http');
-const httpProxy = require('http-proxy');
-const config = require('config');
-const db = require('./db');
-const port = config.get('port');
-const proxyOptions = {...config.proxy};
-const authToken = config.get('authToken');
-const checkTtl = config.get('checkTtl');
+const http = require('http')
+const httpProxy = require('http-proxy')
+const fetch = require('node-fetch')
+const config = require('config')
+const db = require('./db')
+const port = config.get('port')
+const proxyOptions = {...config.proxy}
+const authToken = config.get('authToken')
+const checkTtl = config.get('checkTtl')
 
 //
 // Create your proxy server and set the target in the options.
 //
-const proxy = httpProxy.createProxyServer(proxyOptions);
+// TODO remove proxy and use fetch for everything
+const proxy = httpProxy.createProxyServer(proxyOptions)
 
 // Received response event
 proxy.on('proxyRes', (proxyRes, req, res) => {
-  let chunks = [];
+  let chunks = []
 
   proxyRes.on('data', (chunk) => {
-    chunks.push(chunk);
-  });
+    chunks.push(chunk)
+  })
 
   proxyRes.on('end', () => {
     // Skip response if statusCode isn't equal 200
     if (proxyRes.statusCode !== 200)
-      return;
+      return
 
-    let body = [...Buffer.concat(chunks)];
+    // TODO check the option to convert body to BSON before saving to DB
+    let body = [...Buffer.concat(chunks)]
     db.writeCache({
       url: req.url,
       method: req.method,
       statusCode: proxyRes.statusCode,
       resHeaders: proxyRes.headers,
       resBody: body
-    });
-  });
-});
+    })
+  })
+})
 
 //
 // Create your target server
@@ -42,37 +45,61 @@ proxy.on('proxyRes', (proxyRes, req, res) => {
 http.createServer(async (req, res) => {
   // Authorization
   if (authToken && req.headers['authorization'] !== authToken) {
-      res.statusCode = 401;
-      res.end();
-      return
+    res.statusCode = 403
+    res.end()
+    return
   }
 
   // Read cache
-  const cache = await db.readCache(req.method, req.url);
+  let cache = await db.readCache(req.method, req.url)
 
   if (!cache)
-    proxy.web(req, res);
+    proxy.web(req, res)
   else {
     // Check TTL
-    if (checkTtl){
-      const cacheDate = new Date(cache.date);
-      const isExpired = Date.now() - cacheDate.getTime() > cache.ttl;
-      if (isExpired){
-        proxy.web(req,  res);
-        return
+    if (checkTtl) {
+      const cacheDate = new Date(cache.date)
+      const isExpired = Date.now() - cacheDate.getTime() > cache.ttl
+      if (isExpired) {
+        try {
+          // Call target url
+          const proxyRes = await fetch(`${proxyOptions.target}${req.url}`, {
+            method: req.method,
+            headers: {...req.headers, host: 'acat.online', authorization: proxyOptions.headers.Authorization},
+            // TODO implement body support
+            body: null
+          })
+
+          // If the call is successful then replace cache
+          if (proxyRes.status === 200) {
+            const body = await proxyRes.buffer()
+            const resHeaders = {}
+            proxyRes.headers.forEach((value, key, map) => {
+              resHeaders[key] = value
+            })
+            cache = {
+              url: req.url,
+              method: req.method,
+              statusCode: proxyRes.status,
+              resHeaders,
+              resBody: [...body]
+            }
+            db.writeCache(cache)
+          }
+        } catch (err) {
+          // don't care about error at the moment
+        }
       }
     }
 
-    res.statusCode = cache.statusCode;
-
-    const contentType = cache.resHeaders['content-type'];
-    if (contentType) res.setHeader('content-type', contentType);
-
-    const contentEncoding = cache.resHeaders['content-encoding'];
-    if (contentEncoding) res.setHeader('content-encoding', contentEncoding);
-    res.end(Buffer.from(cache.resBody));
+    res.statusCode = cache.statusCode
+    const contentType = cache.resHeaders['content-type']
+    if (contentType) res.setHeader('content-type', contentType)
+    const contentEncoding = cache.resHeaders['content-encoding']
+    if (contentEncoding) res.setHeader('content-encoding', contentEncoding)
+    res.end(Buffer.from(cache.resBody))
   }
   // proxy.web(req, res);
-}).listen(port);
+}).listen(port)
 
-console.log(`Server started at ${port}`);
+console.log(`Server started at ${port}`)
